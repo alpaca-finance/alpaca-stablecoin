bc2160013bdd0a498853eff1d330a775744076b0
jakeal-papaca <jakeal.papaca@gmail.com>
2021-08-18 15:08:35 +0700

    feat: apply reentrancy guard to StablecoinAdapter  contract

--- contracts/6.12/stablecoin-core/adapters/StablecoinAdapter.sol
+++ contracts/6.12/stablecoin-core/adapters/StablecoinAdapter.sol
@@ -22,6 +22,7 @@ pragma solidity 0.6.12;
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
 
 // FIXME: This contract was altered compared to the production version.
 // It doesn't use LibNote anymore.
@@ -83,7 +84,12 @@ interface GovernmentLike {
 
 */
 
-contract StablecoinAdapter is OwnableUpgradeable, PausableUpgradeable, AccessControlUpgradeable {
+contract StablecoinAdapter is
+  OwnableUpgradeable,
+  PausableUpgradeable,
+  AccessControlUpgradeable,
+  ReentrancyGuardUpgradeable
+{
   // --- Auth ---
   mapping(address => uint256) public wards;
 
@@ -108,6 +114,7 @@ contract StablecoinAdapter is OwnableUpgradeable, PausableUpgradeable, AccessCon
     OwnableUpgradeable.__Ownable_init();
     PausableUpgradeable.__Pausable_init();
     AccessControlUpgradeable.__AccessControl_init();
+    ReentrancyGuardUpgradeable.__ReentrancyGuard_init();
 
     wards[msg.sender] = 1;
     live = 1;
