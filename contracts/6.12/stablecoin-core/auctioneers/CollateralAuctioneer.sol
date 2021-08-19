b115c31eef3d39664f562fa145256f32a0776a39
jakeal-papaca <jakeal.papaca@gmail.com>
2021-08-19 18:06:38 +0700

    refactor: apply IPriceOracle to CollateralAuctioneer

--- contracts/6.12/stablecoin-core/auctioneers/CollateralAuctioneer.sol
+++ contracts/6.12/stablecoin-core/auctioneers/CollateralAuctioneer.sol
@@ -26,6 +26,7 @@ import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol
 
 import "../../interfaces/IBookKeeper.sol";
 import "../../interfaces/IPriceFeed.sol";
+import "../../interfaces/IPriceOracle.sol";
 
 interface GovernmentLike {
   function moveStablecoin(
@@ -56,12 +57,6 @@ interface GovernmentLike {
     address,
     uint256
   ) external;
-}
-
-interface PriceOracleLike {
-  function stableCoinReferencePrice() external returns (uint256);
-
-  function collateralPools(bytes32) external returns (IPriceFeed, uint256);
 }
 
 interface LiquidationEngineLike {
@@ -113,7 +108,7 @@ contract CollateralAuctioneer is
 
   LiquidationEngineLike public liquidationEngine; // Liquidation module
   address public systemDebtEngine; // Recipient of dai raised in auctions
-  PriceOracleLike public priceOracle; // Collateral price module
+  IPriceOracle public priceOracle; // Collateral price module
   CalculatorLike public calc; // Current price calculator
 
   uint256 public startingPriceBuffer; // Multiplicative factor to increase starting price                  [ray]
@@ -195,7 +190,7 @@ contract CollateralAuctioneer is
     ReentrancyGuardUpgradeable.__ReentrancyGuard_init();
 
     bookKeeper = IBookKeeper(_bookKeeper);
-    priceOracle = PriceOracleLike(priceOracle_);
+    priceOracle = IPriceOracle(priceOracle_);
     liquidationEngine = LiquidationEngineLike(liquidationEngine_);
     collateralPoolId = collateralPoolId_;
     startingPriceBuffer = RAY;
@@ -234,7 +229,7 @@ contract CollateralAuctioneer is
   }
 
   function file(bytes32 what, address data) external auth lock {
-    if (what == "priceOracle") priceOracle = PriceOracleLike(data);
+    if (what == "priceOracle") priceOracle = IPriceOracle(data);
     else if (what == "liquidationEngine") liquidationEngine = LiquidationEngineLike(data);
     else if (what == "systemDebtEngine") systemDebtEngine = data;
     else if (what == "calc") calc = CalculatorLike(data);
