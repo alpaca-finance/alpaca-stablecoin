52da8528b4d668dddd7cee3c43fe87d716786107
Almost Llamas <kim.tony.20.20.20@gmail.com>
2021-08-19 16:41:54 +0700

    test: first test case

---
+++ test/units/managers/CDPManager.test.ts
@@ -0,0 +1,76 @@
+import { ethers, upgrades, waffle } from "hardhat"
+import { Signer, BigNumber, Wallet } from "ethers"
+import chai from "chai"
+import { MockProvider, solidity } from "ethereum-waffle"
+import "@openzeppelin/test-helpers"
+import { CDPManager, CDPManager__factory } from "../../../typechain"
+import { ModifiableContract, smoddit } from "@eth-optimism/smock"
+
+chai.use(solidity)
+const { expect } = chai
+const { AddressZero } = ethers.constants
+
+describe("CDPManager", () => {
+  // Accounts
+  let deployer: Signer
+  let alice: Signer
+  let bob: Signer
+  let dev: Signer
+
+  // Account Addresses
+  let deployerAddress: string
+  let aliceAddress: string
+  let bobAddress: string
+  let devAddress: string
+
+  // Contracts
+  let cdpManager: CDPManager
+  let mockedBookKeeper: ModifiableContract
+
+  beforeEach(async () => {
+    type fixture = {
+      cdpManager: CDPManager
+      mockedBookKeeper: ModifiableContract
+    }
+    ;({ cdpManager, mockedBookKeeper } = await waffle.loadFixture(
+      async (maybeWallets?: Wallet[], maybeProvider?: MockProvider): Promise<fixture> => {
+        // Deploy mocked booster config
+        const BookKeeperFactory = await smoddit("BookKeeper", deployer)
+        const mockedBookKeeper = await BookKeeperFactory.deploy()
+
+        // Deploy CDPManager
+        const CDPManager = (await ethers.getContractFactory("CDPManager", deployer)) as CDPManager__factory
+        const cdpManager = (await upgrades.deployProxy(CDPManager, [mockedBookKeeper.address])) as CDPManager
+        await cdpManager.deployed()
+
+        return { cdpManager, mockedBookKeeper }
+      }
+    ))
+    ;[deployer, alice, bob, dev] = await ethers.getSigners()
+    ;[deployerAddress, aliceAddress, bobAddress, devAddress] = await Promise.all([
+      deployer.getAddress(),
+      alice.getAddress(),
+      bob.getAddress(),
+      dev.getAddress(),
+    ])
+  })
+
+  describe("#open()", () => {
+    it("should be able to open CDP with an incremental CDP index", async () => {
+      expect(await cdpManager.owns(1)).to.equal(AddressZero)
+      await cdpManager.open(ethers.utils.formatBytes32String("BNB"), aliceAddress)
+      expect(await cdpManager.cdpi()).to.bignumber.equal(1)
+      expect(await cdpManager.owns(1)).to.equal(aliceAddress)
+
+      expect(await cdpManager.owns(2)).to.equal(AddressZero)
+      await cdpManager.open(ethers.utils.formatBytes32String("BNB"), bobAddress)
+      expect(await cdpManager.cdpi()).to.bignumber.equal(2)
+      expect(await cdpManager.owns(2)).to.equal(bobAddress)
+
+      expect(await cdpManager.owns(3)).to.equal(AddressZero)
+      await cdpManager.open(ethers.utils.formatBytes32String("COL"), aliceAddress)
+      expect(await cdpManager.cdpi()).to.bignumber.equal(3)
+      expect(await cdpManager.owns(3)).to.equal(aliceAddress)
+    })
+  })
+})
