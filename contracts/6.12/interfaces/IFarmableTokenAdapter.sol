d8b6e6cc517f4a28a150a13207881dc5123ae9d8
jakeal-papaca <jakeal.papaca@gmail.com>
2021-08-20 18:18:56 +0700

    refactor: extract FarmableTokenAdapterLike to IFarmableTokenAdapter interface

--- contracts/6.12/interfaces/IFarmableTokenAdapter.sol
+++ contracts/6.12/interfaces/IFarmableTokenAdapter.sol
@@ -1,7 +1,15 @@
 pragma solidity 0.6.12;
 
+import "../interfaces/IToken.sol";
+
 interface IFarmableTokenAdapter {
   function deposit(
+    address,
+    address,
+    uint256
+  ) external;
+
+  function withdraw(
     address,
     address,
     uint256
@@ -14,4 +22,6 @@ interface IFarmableTokenAdapter {
   ) external;
 
   function collateralPoolId() external view returns (bytes32);
+
+  function collateralToken() external returns (IToken);
 }
