e9d6f35e18fd78e06bfce4d05351b7db9f2c6029
Almost Llamas <kim.tony.20.20.20@gmail.com>
2021-08-20 19:00:23 +0700

    test: add unit test for addCollateral

--- test/units/managers/CDPManager.test.ts
+++ test/units/managers/CDPManager.test.ts
@@ -9,7 +9,7 @@ import { ModifiableContract, smoddit, smockit, MockContract } from "@eth-optimis
 chai.use(solidity)
 const { expect } = chai
 const { AddressZero, WeiPerEther } = ethers.constants
-const { parseEther } = ethers.utils
+const { parseEther, formatBytes32String } = ethers.utils
 
 type fixture = {
   cdpManager: CDPManager
@@ -68,25 +68,23 @@ describe("CDPManager", () => {
   describe("#open()", () => {
     context("when supply zero address", () => {
       it("should revert", async () => {
-        await expect(cdpManager.open(ethers.utils.formatBytes32String("BNB"), AddressZero)).to.be.revertedWith(
-          "usr-address-0"
-        )
+        await expect(cdpManager.open(formatBytes32String("BNB"), AddressZero)).to.be.revertedWith("usr-address-0")
       })
     })
     context("when parameters are valid", () => {
       it("should be able to open CDP with an incremental CDP index", async () => {
         expect(await cdpManager.owns(1)).to.equal(AddressZero)
-        await cdpManager.open(ethers.utils.formatBytes32String("BNB"), aliceAddress)
+        await cdpManager.open(formatBytes32String("BNB"), aliceAddress)
         expect(await cdpManager.cdpi()).to.bignumber.equal(1)
         expect(await cdpManager.owns(1)).to.equal(aliceAddress)
 
         expect(await cdpManager.owns(2)).to.equal(AddressZero)
-        await cdpManager.open(ethers.utils.formatBytes32String("BNB"), bobAddress)
+        await cdpManager.open(formatBytes32String("BNB"), bobAddress)
         expect(await cdpManager.cdpi()).to.bignumber.equal(2)
         expect(await cdpManager.owns(2)).to.equal(bobAddress)
 
         expect(await cdpManager.owns(3)).to.equal(AddressZero)
-        await cdpManager.open(ethers.utils.formatBytes32String("COL"), aliceAddress)
+        await cdpManager.open(formatBytes32String("COL"), aliceAddress)
         expect(await cdpManager.cdpi()).to.bignumber.equal(3)
         expect(await cdpManager.owns(3)).to.equal(aliceAddress)
       })
@@ -96,25 +94,25 @@ describe("CDPManager", () => {
   describe("#give()", () => {
     context("when caller is not the owner of the cdp (or have no allowance)", () => {
       it("should revert", async () => {
-        await cdpManager.open(ethers.utils.formatBytes32String("BNB"), aliceAddress)
+        await cdpManager.open(formatBytes32String("BNB"), aliceAddress)
         await expect(cdpManager.give(1, aliceAddress)).to.be.revertedWith("cdp-not-allowed")
       })
     })
     context("when input destination as zero address", () => {
       it("should revert", async () => {
-        await cdpManager.open(ethers.utils.formatBytes32String("BNB"), aliceAddress)
+        await cdpManager.open(formatBytes32String("BNB"), aliceAddress)
         await expect(cdpManagerAsAlice.give(1, AddressZero)).to.be.revertedWith("dst-address-0")
       })
     })
     context("when input destination as current owner address", () => {
       it("should revert", async () => {
-        await cdpManager.open(ethers.utils.formatBytes32String("BNB"), aliceAddress)
+        await cdpManager.open(formatBytes32String("BNB"), aliceAddress)
         await expect(cdpManagerAsAlice.give(1, aliceAddress)).to.be.revertedWith("dst-already-owner")
       })
     })
     context("when parameters are valid", () => {
       it("should be able to change the owner of CDP ", async () => {
-        await cdpManager.open(ethers.utils.formatBytes32String("BNB"), aliceAddress)
+        await cdpManager.open(formatBytes32String("BNB"), aliceAddress)
         expect(await cdpManager.owns(1)).to.equal(aliceAddress)
         await cdpManagerAsAlice.give(1, bobAddress)
         expect(await cdpManager.owns(1)).to.equal(bobAddress)
@@ -125,13 +123,13 @@ describe("CDPManager", () => {
   describe("#cdpAllow()", () => {
     context("when caller is not the owner of the cdp (or have no allowance)", () => {
       it("should revert", async () => {
-        await cdpManager.open(ethers.utils.formatBytes32String("BNB"), aliceAddress)
+        await cdpManager.open(formatBytes32String("BNB"), aliceAddress)
         await expect(cdpManager.cdpAllow(1, aliceAddress, 1)).to.be.revertedWith("cdp-not-allowed")
       })
     })
     context("when parameters are valid", () => {
       it("should be able to add user allowance to a cdp", async () => {
-        await cdpManager.open(ethers.utils.formatBytes32String("BNB"), aliceAddress)
+        await cdpManager.open(formatBytes32String("BNB"), aliceAddress)
         expect(await cdpManager.cdpCan(aliceAddress, 1, bobAddress)).to.bignumber.equal(0)
         await cdpManagerAsAlice.cdpAllow(1, bobAddress, 1)
         expect(await cdpManager.cdpCan(aliceAddress, 1, bobAddress)).to.bignumber.equal(1)
@@ -143,15 +141,15 @@ describe("CDPManager", () => {
     context("when a few cdp has been opened", () => {
       it("should work as a linklist perfectly", async () => {
         // Alice open cdp 1-3
-        await cdpManager.open(ethers.utils.formatBytes32String("BNB"), aliceAddress)
-        await cdpManager.open(ethers.utils.formatBytes32String("BNB"), aliceAddress)
-        await cdpManager.open(ethers.utils.formatBytes32String("BNB"), aliceAddress)
+        await cdpManager.open(formatBytes32String("BNB"), aliceAddress)
+        await cdpManager.open(formatBytes32String("BNB"), aliceAddress)
+        await cdpManager.open(formatBytes32String("BNB"), aliceAddress)
 
         // Bob open cdp 4-7
-        await cdpManager.open(ethers.utils.formatBytes32String("BNB"), bobAddress)
-        await cdpManager.open(ethers.utils.formatBytes32String("BNB"), bobAddress)
-        await cdpManager.open(ethers.utils.formatBytes32String("BNB"), bobAddress)
-        await cdpManager.open(ethers.utils.formatBytes32String("BNB"), bobAddress)
+        await cdpManager.open(formatBytes32String("BNB"), bobAddress)
+        await cdpManager.open(formatBytes32String("BNB"), bobAddress)
+        await cdpManager.open(formatBytes32String("BNB"), bobAddress)
+        await cdpManager.open(formatBytes32String("BNB"), bobAddress)
 
         let [aliceCount, aliceFirst, aliceLast] = await Promise.all([
           cdpManager.count(aliceAddress),
@@ -210,28 +208,93 @@ describe("CDPManager", () => {
   describe("#adjustPosition()", () => {
     context("when caller is not the owner of the cdp (or have no allowance)", () => {
       it("should revert", async () => {
-        await cdpManager.open(ethers.utils.formatBytes32String("BNB"), aliceAddress)
-        await expect(cdpManager.adjustPosition(1, parseEther("2"), parseEther("1"))).to.be.revertedWith(
+        await cdpManager.open(formatBytes32String("BNB"), aliceAddress)
+        await expect(cdpManager.adjustPosition(1, parseEther("1"), parseEther("50"))).to.be.revertedWith(
           "cdp-not-allowed"
         )
       })
     })
     context("when parameters are valid", async () => {
       it("should be able to call BookKeeper.adjustPosition", async () => {
-        await cdpManager.open(ethers.utils.formatBytes32String("BNB"), aliceAddress)
+        await cdpManager.open(formatBytes32String("BNB"), aliceAddress)
         const positionAddress = await cdpManager.positions(1)
 
         mockedBookKeeper.smocked.adjustPosition.will.return.with()
-        await cdpManagerAsAlice.adjustPosition(1, parseEther("2"), parseEther("1"))
+        await cdpManagerAsAlice.adjustPosition(1, parseEther("1"), parseEther("50"))
 
         const { calls } = mockedBookKeeper.smocked.adjustPosition
         expect(calls.length).to.be.equal(1)
-        expect(calls[0].collateralPoolId).to.be.equal(ethers.utils.formatBytes32String("BNB"))
+        expect(calls[0].collateralPoolId).to.be.equal(formatBytes32String("BNB"))
         expect(calls[0].positionAddress).to.be.equal(positionAddress)
         expect(calls[0].collateralOwner).to.be.equal(positionAddress)
         expect(calls[0].stablecoinOwner).to.be.equal(positionAddress)
-        expect(calls[0].collateralValue).to.be.equal(parseEther("2"))
-        expect(calls[0].debtShare).to.be.equal(parseEther("1"))
+        expect(calls[0].collateralValue).to.be.equal(parseEther("1"))
+        expect(calls[0].debtShare).to.be.equal(parseEther("50"))
+      })
+    })
+  })
+
+  describe("#moveCollateral(uint256,address,uint256)", () => {
+    context("when caller is not the owner of the cdp", () => {
+      it("should revert", async () => {
+        await cdpManager.open(formatBytes32String("BNB"), aliceAddress)
+        await expect(
+          cdpManager["moveCollateral(uint256,address,uint256)"](1, aliceAddress, parseEther("50"))
+        ).to.be.revertedWith("cdp-not-allowed")
+      })
+    })
+    context("when parameters are valid", async () => {
+      it("should be able to call moveCollateral(uint256,address,uint256)", async () => {
+        await cdpManager.open(formatBytes32String("BNB"), aliceAddress)
+        const positionAddress = await cdpManager.positions(1)
+
+        mockedBookKeeper.smocked.moveCollateral.will.return.with()
+        await cdpManagerAsAlice["moveCollateral(uint256,address,uint256)"](1, bobAddress, parseEther("1"))
+
+        const { calls } = mockedBookKeeper.smocked.moveCollateral
+        expect(calls.length).to.be.equal(1)
+        expect(calls[0].collateralPoolId).to.be.equal(formatBytes32String("BNB"))
+        expect(calls[0].src).to.be.equal(positionAddress)
+        expect(calls[0].dst).to.be.equal(bobAddress)
+        expect(calls[0].wad).to.be.equal(parseEther("1"))
+      })
+    })
+  })
+
+  // This function has the purpose to take away collateral from the system that doesn't correspond to the cdp but was sent there wrongly.
+  describe("#moveCollateral(bytes32,uint256,address,uint256)", () => {
+    context("when caller is not the owner of the cdp", () => {
+      it("should revert", async () => {
+        await cdpManager.open(formatBytes32String("BNB"), aliceAddress)
+        await expect(
+          cdpManager["moveCollateral(bytes32,uint256,address,uint256)"](
+            formatBytes32String("BNB"),
+            1,
+            aliceAddress,
+            parseEther("50")
+          )
+        ).to.be.revertedWith("cdp-not-allowed")
+      })
+    })
+    context("when parameters are valid", async () => {
+      it("should be able to call moveCollateral(bytes32,uint256,address,uint256)", async () => {
+        await cdpManager.open(formatBytes32String("BNB"), aliceAddress)
+        const positionAddress = await cdpManager.positions(1)
+
+        mockedBookKeeper.smocked.moveCollateral.will.return.with()
+        await cdpManagerAsAlice["moveCollateral(bytes32,uint256,address,uint256)"](
+          formatBytes32String("BNB"),
+          1,
+          bobAddress,
+          parseEther("1")
+        )
+
+        const { calls } = mockedBookKeeper.smocked.moveCollateral
+        expect(calls.length).to.be.equal(1)
+        expect(calls[0].collateralPoolId).to.be.equal(formatBytes32String("BNB"))
+        expect(calls[0].src).to.be.equal(positionAddress)
+        expect(calls[0].dst).to.be.equal(bobAddress)
+        expect(calls[0].wad).to.be.equal(parseEther("1"))
       })
     })
   })
