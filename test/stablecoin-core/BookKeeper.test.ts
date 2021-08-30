30594dc7dc87eccb37c02ae6e46ffbadab39f845
jakeal-papaca <jakeal.papaca@gmail.com>
2021-08-30 15:15:13 +0700

    test: add unit test for init

--- test/stablecoin-core/BookKeeper.test.ts
+++ test/stablecoin-core/BookKeeper.test.ts
@@ -1,16 +1,14 @@
 import { ethers, upgrades, waffle } from "hardhat"
-import { Signer, BigNumber, Wallet } from "ethers"
+import { Signer } from "ethers"
 import chai from "chai"
-import { MockProvider, solidity } from "ethereum-waffle"
+import { solidity } from "ethereum-waffle"
 import "@openzeppelin/test-helpers"
-import { BookKeeper__factory, CDPManager, CDPManager__factory, BookKeeper } from "../../typechain"
-import { smockit, MockContract } from "@eth-optimism/smock"
+import { BookKeeper__factory, BookKeeper } from "../../typechain"
 import { WeiPerRad, WeiPerRay, WeiPerWad } from "../helper/unit"
 
 chai.use(solidity)
 const { expect } = chai
-const { AddressZero } = ethers.constants
-const { parseEther, formatBytes32String } = ethers.utils
+const { formatBytes32String } = ethers.utils
 
 type fixture = {
   bookKeeper: BookKeeper
@@ -80,139 +78,6 @@ describe("BookKeeper", () => {
             "BookKeeper/collateral-pool-already-init"
           )
         })
-      })
-    })
-  })
-
-  describe("#addCollateral", () => {
-    context("when the caller is not the owner", async () => {
-      it("should revert", async () => {
-        await expect(
-          bookKeeperAsAlice.addCollateral(formatBytes32String("BNB"), deployerAddress, WeiPerWad)
-        ).to.be.revertedWith("BookKeeper/not-authorized")
-      })
-    })
-    context("when the caller is the owner", async () => {
-      context("when collateral to add is positive", () => {
-        it("should be able to call addCollateral", async () => {
-          // init BNB collateral pool
-          await bookKeeper.init(formatBytes32String("BNB"))
-
-          const collateralTokenBefore = await bookKeeper.collateralToken(formatBytes32String("BNB"), deployerAddress)
-          expect(collateralTokenBefore).to.be.equal(0)
-
-          await bookKeeper.addCollateral(formatBytes32String("BNB"), deployerAddress, WeiPerWad)
-
-          const collateralTokenAfter = await bookKeeper.collateralToken(formatBytes32String("BNB"), deployerAddress)
-          expect(collateralTokenAfter).to.be.equal(WeiPerWad)
-        })
-      })
-
-      context("when collateral to add is negative", () => {
-        it("should be able to call addCollateral", async () => {
-          // init BNB collateral pool
-          await bookKeeper.init(formatBytes32String("BNB"))
-
-          // add collateral 1 BNB
-          await bookKeeper.addCollateral(formatBytes32String("BNB"), deployerAddress, WeiPerWad)
-
-          const collateralTokenBefore = await bookKeeper.collateralToken(formatBytes32String("BNB"), deployerAddress)
-          expect(collateralTokenBefore).to.be.equal(WeiPerWad)
-
-          // add collateral -1 BNB
-          await bookKeeper.addCollateral(formatBytes32String("BNB"), deployerAddress, WeiPerWad.mul(-1))
-
-          const collateralTokenAfter = await bookKeeper.collateralToken(formatBytes32String("BNB"), deployerAddress)
-          expect(collateralTokenAfter).to.be.equal(0)
-        })
-      })
-    })
-  })
-
-  describe("#moveCollateral", () => {
-    context("when the caller is not the owner", () => {
-      it("should be revert", async () => {
-        // bob call move collateral from alice to bob
-        await await expect(
-          bookKeeperAsBob.moveCollateral(formatBytes32String("BNB"), aliceAddress, bobAddress, WeiPerWad)
-        ).to.be.revertedWith("BookKeeper/not-allowed")
-      })
-
-      context("when alice allow bob to move collateral", () => {
-        it("should be able to call moveCollateral", async () => {
-          // add collateral 1 BNB to alice
-          await bookKeeper.addCollateral(formatBytes32String("BNB"), aliceAddress, WeiPerWad)
-
-          const collateralTokenAliceBefore = await bookKeeper.collateralToken(formatBytes32String("BNB"), aliceAddress)
-          expect(collateralTokenAliceBefore).to.be.equal(WeiPerWad)
-          const collateralTokenBobBefore = await bookKeeper.collateralToken(formatBytes32String("BNB"), bobAddress)
-          expect(collateralTokenBobBefore).to.be.equal(0)
-
-          // alice allow bob to move collateral
-          await bookKeeperAsAlice.hope(bobAddress)
-
-          // bob call move collateral from alice to bob
-          await bookKeeperAsBob.moveCollateral(formatBytes32String("BNB"), aliceAddress, bobAddress, WeiPerWad)
-
-          const collateralTokenAliceAfter = await bookKeeper.collateralToken(formatBytes32String("BNB"), aliceAddress)
-          expect(collateralTokenAliceAfter).to.be.equal(0)
-          const collateralTokenBobAfter = await bookKeeper.collateralToken(formatBytes32String("BNB"), bobAddress)
-          expect(collateralTokenBobAfter).to.be.equal(WeiPerWad)
-        })
-      })
-    })
-
-    context("when the caller is the owner", () => {
-      it("should be able to call moveCollateral", async () => {
-        // init BNB collateral pool
-        await bookKeeper.init(formatBytes32String("BNB"))
-
-        // add collateral 1 BNB to alice
-        await bookKeeper.addCollateral(formatBytes32String("BNB"), aliceAddress, WeiPerWad)
-
-        const collateralTokenAliceBefore = await bookKeeper.collateralToken(formatBytes32String("BNB"), aliceAddress)
-        expect(collateralTokenAliceBefore).to.be.equal(WeiPerWad)
-        const collateralTokenBobBefore = await bookKeeper.collateralToken(formatBytes32String("BNB"), bobAddress)
-        expect(collateralTokenBobBefore).to.be.equal(0)
-
-        // move collateral 1 BNB from alice to bob
-        await bookKeeperAsAlice.moveCollateral(formatBytes32String("BNB"), aliceAddress, bobAddress, WeiPerWad)
-
-        const collateralTokenAliceAfter = await bookKeeper.collateralToken(formatBytes32String("BNB"), aliceAddress)
-        expect(collateralTokenAliceAfter).to.be.equal(0)
-        const collateralTokenBobAfter = await bookKeeper.collateralToken(formatBytes32String("BNB"), bobAddress)
-        expect(collateralTokenBobAfter).to.be.equal(WeiPerWad)
-      })
-    })
-  })
-
-  describe("#adjustPosition", () => {
-    context("when parameters are valid", () => {
-      it("should be able to call adjustPosition", async () => {
-        await bookKeeper.init(formatBytes32String("BNB"))
-        await bookKeeper.addCollateral(formatBytes32String("BNB"), deployerAddress, WeiPerWad)
-
-        const collateralTokenBefore = await bookKeeper.collateralToken(formatBytes32String("BNB"), deployerAddress)
-        const positionBefore = await bookKeeper.positions(formatBytes32String("BNB"), deployerAddress)
-
-        expect(collateralTokenBefore).to.be.equal(WeiPerWad)
-        expect(positionBefore.lockedCollateral).to.be.equal(0)
-
-        await bookKeeper.adjustPosition(
-          formatBytes32String("BNB"),
-          deployerAddress,
-          deployerAddress,
-          deployerAddress,
-          WeiPerWad,
-          0
-        )
-
-        const collateralTokenAfter = await bookKeeper.collateralToken(formatBytes32String("BNB"), deployerAddress)
-        const positionAfter = await bookKeeper.positions(formatBytes32String("BNB"), deployerAddress)
-
-        expect(collateralTokenAfter).to.be.equal(0)
-        expect(positionAfter.lockedCollateral).to.be.equal(WeiPerWad)
-        // expect()
       })
     })
   })
