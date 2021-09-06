1c40b272ddf35bdc7e427c66d3db40c7d5794f3f
jakeal-papaca <jakeal.papaca@gmail.com>
2021-09-06 11:36:01 +0700

    fix: create ProxyWalletCache file

--- contracts/6.12/proxy-wallet/ProxyWallet.sol
+++ contracts/6.12/proxy-wallet/ProxyWallet.sol
@@ -19,6 +19,7 @@ pragma solidity 0.6.12;
 
 import "./AlpacaAuth.sol";
 import "./AlpacaNote.sol";
+import "./ProxyWalletCache.sol";
 
 // ProxyWallet
 // Allows code execution using a persistant identity This can be very
@@ -75,64 +76,5 @@ contract ProxyWallet is AlpacaAuth, AlpacaNote {
     require(_cacheAddr != address(0), "proxy-wallet-cache-address-required");
     cache = ProxyWalletCache(_cacheAddr); // overwrite cache
     return true;
-  }
-}
-
-// ProxyWalletFactory
-// This factory deploys new proxy instances through build()
-// Deployed proxy addresses are logged
-contract ProxyWalletFactory {
-  event Created(address indexed sender, address indexed owner, address proxy, address cache);
-  mapping(address => bool) public isProxy;
-  ProxyWalletCache public cache;
-
-  constructor() public {
-    cache = new ProxyWalletCache();
-  }
-
-  // deploys a new proxy instance
-  // sets owner of proxy to caller
-  function build() public returns (address payable proxy) {
-    proxy = build(msg.sender);
-  }
-
-  // deploys a new proxy instance
-  // sets custom owner of proxy
-  function build(address owner) public returns (address payable proxy) {
-    proxy = address(new ProxyWallet(address(cache)));
-    emit Created(msg.sender, owner, address(proxy), address(cache));
-    ProxyWallet(proxy).setOwner(owner);
-    isProxy[proxy] = true;
-  }
-}
-
-// ProxyWalletCache
-// This global cache stores addresses of contracts previously deployed
-// by a proxy. This saves gas from repeat deployment of the same
-// contracts and eliminates blockchain bloat.
-
-// By default, all proxies deployed from the same factory store
-// contracts in the same cache. The cache a proxy instance uses can be
-// changed.  The cache uses the sha3 hash of a contract's bytecode to
-// lookup the address
-contract ProxyWalletCache {
-  mapping(bytes32 => address) cache;
-
-  function read(bytes memory _code) public view returns (address) {
-    bytes32 hash = keccak256(_code);
-    return cache[hash];
-  }
-
-  function write(bytes memory _code) public returns (address target) {
-    assembly {
-      target := create(0, add(_code, 0x20), mload(_code))
-      switch iszero(extcodesize(target))
-        case 1 {
-          // throw if contract failed to deploy
-          revert(0, 0)
-        }
-    }
-    bytes32 hash = keccak256(_code);
-    cache[hash] = target;
   }
 }
