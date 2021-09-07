017f435ba905be1d82a17ccb1827a1c2caf28a39
sundayalpaca <sundayalpaca@gmail.com>
2021-09-07 18:45:39 +0700

    feat : refactor setter and revise unit test stability fee collector

--- contracts/6.12/interfaces/IStabilityFeeCollector.sol
+++ contracts/6.12/interfaces/IStabilityFeeCollector.sol
@@ -2,4 +2,10 @@ pragma solidity 0.6.12;
 
 interface IStabilityFeeCollector {
   function collect(bytes32) external returns (uint256);
+
+  function setGlobalStabilityFeeRate(uint256) external;
+
+  function setSystemDebtEngine(address) external;
+
+  function setStabilityFeeRate(bytes32, uint256) external;
 }
