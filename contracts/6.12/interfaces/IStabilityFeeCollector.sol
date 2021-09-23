82114cdb94ec6f4a7ceb372a9ee2cfd25a349f93
Almost Llamas <78682880+almost-llamas@users.noreply.github.com>
2021-09-23 14:52:31 +0700

    Update contracts/6.12/interfaces/IStabilityFeeCollector.sol
    Co-authored-by: Ricardo Alpaca <ricardo.alpaca.619@gmail.com>

--- contracts/6.12/interfaces/IStabilityFeeCollector.sol
+++ contracts/6.12/interfaces/IStabilityFeeCollector.sol
@@ -1,5 +1,5 @@
 pragma solidity 0.6.12;
 
 interface IStabilityFeeCollector {
-  function collect(bytes32 collateralPoolId) external returns (uint256 debtAccumulatedRate);
+  function collect(bytes32 collateralPoolId) external returns (uint256 debtAccumulatedRate); // [ray]
 }
