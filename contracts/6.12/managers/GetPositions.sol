8bc63111427371f96c8d666b6db61959d88a0ae6
ricardo.alpaca <ricardo.alpaca.619@gmail.com>
2021-10-05 10:57:05 +0700

    feat: add method to query positions with safety buffer

--- contracts/6.12/managers/GetPositions.sol
+++ contracts/6.12/managers/GetPositions.sol
@@ -10,6 +10,11 @@ import "../interfaces/IBookKeeper.sol";
 contract GetPositions is Initializable {
   using SafeMathUpgradeable for uint256;
 
+  // --- Math ---
+  uint256 constant WAD = 10**18;
+  uint256 constant RAY = 10**27;
+  uint256 constant RAD = 10**45;
+
   // --- Init ---
   function initialize() external initializer {}
 
@@ -63,46 +68,66 @@ contract GetPositions is Initializable {
     }
   }
 
-  function getPositionsAtRisk(
+  function getPositionWithSafetyBuffer(
     address _manager,
-    uint256 _safetyBufferThreshold,
     uint256 _startIndex,
     uint256 _offset
   )
     external
     view
     returns (
-      uint256[] memory ids,
       address[] memory positions,
-      bytes32[] memory collateralPools,
-      uint256[] memory lockedCollaterals,
       uint256[] memory debtShares,
-      uint256[] memory safetyBuffer
+      uint256[] memory safetyBuffers
     )
   {
+    if (_startIndex.add(_offset) > PositionManager(_manager).lastPositionId())
+      _offset = PositionManager(_manager).lastPositionId().sub(_startIndex).add(1);
+
     IBookKeeper bookKeeper = IBookKeeper(PositionManager(_manager).bookKeeper());
+    positions = new address[](_offset);
+    debtShares = new uint256[](_offset);
+    safetyBuffers = new uint256[](_offset);
     uint256 _resultIndex = 0;
     for (uint256 _positionIndex = _startIndex; _positionIndex < _startIndex.add(_offset); _positionIndex++) {
-      ids[_resultIndex] = _positionIndex;
+      if (PositionManager(_manager).positions(_positionIndex) == address(0)) break;
       positions[_resultIndex] = PositionManager(_manager).positions(_positionIndex);
-      collateralPools[_resultIndex] = PositionManager(_manager).collateralPools(_positionIndex);
 
-      (lockedCollaterals[_resultIndex], debtShares[_resultIndex]) = bookKeeper.positions(
-        collateralPools[_resultIndex],
+      (uint256 _lockedCollateral, uint256 _debtShare) = bookKeeper.positions(
+        PositionManager(_manager).collateralPools(_positionIndex),
         positions[_resultIndex]
       );
 
       (, uint256 _debtAccumulatedRate, uint256 _priceWithSafetyMargin, , ) = bookKeeper.collateralPools(
-        collateralPools[_resultIndex]
+        PositionManager(_manager).collateralPools(_positionIndex)
       );
 
-      safetyBuffer[_resultIndex] = debtShares[_resultIndex]
-        .mul(_debtAccumulatedRate)
-        .div(lockedCollaterals[_resultIndex])
-        .div(_priceWithSafetyMargin);
-      if (safetyBuffer[_resultIndex] <= _safetyBufferThreshold) {
-        _resultIndex++;
-      }
+      uint256 safetyBuffer = calculateSafetyBuffer(
+        _debtShare,
+        _debtAccumulatedRate,
+        _lockedCollateral,
+        _priceWithSafetyMargin
+      );
+      safetyBuffers[_resultIndex] = safetyBuffer;
+      debtShares[_resultIndex] = _debtShare;
+      _resultIndex++;
     }
+  }
+
+  function calculateSafetyBuffer(
+    uint256 _debtShare, // [wad]
+    uint256 _debtAccumulatedRate, // [ray]
+    uint256 _lockedCollateral, // [wad]
+    uint256 _priceWithSafetyMargin // [ray]
+  )
+    internal
+    view
+    returns (
+      uint256 _safetyBuffer // [rad]
+    )
+  {
+    uint256 collateralValue = _lockedCollateral.mul(_priceWithSafetyMargin);
+    uint256 debtValue = _debtShare.mul(_debtAccumulatedRate);
+    _safetyBuffer = collateralValue >= debtValue ? collateralValue.sub(debtValue) : 0;
   }
 }
