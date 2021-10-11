5f5db05964a28117a3d74afb0cd11f20cb0d4084
jakeal-papaca <jakeal.papaca@gmail.com>
2021-10-11 18:57:37 +0700

    test: fix deploy AlpacaStablecoin contract

--- test/integrations/position-manager/PositionManager.test.ts
+++ test/integrations/position-manager/PositionManager.test.ts
@@ -158,7 +158,11 @@ const loadFixtureHandler = async (): Promise<Fixture> => {
 
   // Deploy AlpacaStablecoin
   const AlpacaStablecoin = new AlpacaStablecoin__factory(deployer)
-  const alpacaStablecoin = await AlpacaStablecoin.deploy("Alpaca USD", "AUSD", "31337")
+  const alpacaStablecoin = (await upgrades.deployProxy(AlpacaStablecoin, [
+    "Alpaca USD",
+    "AUSD",
+    "31337",
+  ])) as AlpacaStablecoin
 
   const BookKeeper = new BookKeeper__factory(deployer)
   const bookKeeper = (await upgrades.deployProxy(BookKeeper)) as BookKeeper
