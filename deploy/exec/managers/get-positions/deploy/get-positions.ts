5c789f9db2326170359a71a1a8419778ab0af17e
jakeal-papaca <jakeal.papaca@gmail.com>
2021-10-12 13:42:55 +0700

    feat: add GetPosition deployment script

---
+++ deploy/exec/managers/get-positions/deploy/get-positions.ts
@@ -0,0 +1,30 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { DeployFunction } from "hardhat-deploy/types"
+import { ethers, upgrades } from "hardhat"
+import { GetPositions__factory } from "../../../../../typechain"
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  /*
+  ░██╗░░░░░░░██╗░█████╗░██████╗░███╗░░██╗██╗███╗░░██╗░██████╗░
+  ░██║░░██╗░░██║██╔══██╗██╔══██╗████╗░██║██║████╗░██║██╔════╝░
+  ░╚██╗████╗██╔╝███████║██████╔╝██╔██╗██║██║██╔██╗██║██║░░██╗░
+  ░░████╔═████║░██╔══██║██╔══██╗██║╚████║██║██║╚████║██║░░╚██╗
+  ░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██║░╚███║██║██║░╚███║╚██████╔╝
+  ░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝╚═╝░░╚══╝░╚═════╝░
+  Check all variables below before execute the deployment script
+  */
+
+  console.log(">> Deploying an upgradable GetPositions contract")
+  const GetPositions = (await ethers.getContractFactory(
+    "GetPositions",
+    (
+      await ethers.getSigners()
+    )[0]
+  )) as GetPositions__factory
+  const getPositions = await upgrades.deployProxy(GetPositions)
+  await getPositions.deployed()
+  console.log(`>> Deployed at ${getPositions.address}`)
+}
+
+export default func
+func.tags = ["GetPositions"]
