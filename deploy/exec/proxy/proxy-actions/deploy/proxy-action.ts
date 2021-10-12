3ecc3d9266f7b7eba78aee292575729d6166ec4e
jakeal-papaca <jakeal.papaca@gmail.com>
2021-10-12 13:35:16 +0700

    feat: add ProxyAction deployment script

--- deploy/exec/proxy/proxy-actions/deploy/proxy-action.ts
+++ deploy/exec/proxy/proxy-actions/deploy/proxy-action.ts
@@ -1,7 +1,7 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types"
 import { DeployFunction } from "hardhat-deploy/types"
 import { ethers, upgrades } from "hardhat"
-import { LiquidationEngine__factory } from "../../../../typechain"
+import { AlpacaStablecoinProxyActions__factory } from "../../../../../typechain"
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   /*
@@ -14,20 +14,17 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   Check all variables below before execute the deployment script
   */
 
-  const BOOK_KEEPER_ADDR = ""
-  const SYSTEM_DEBT_ENGINE_ADDR = ""
-
-  console.log(">> Deploying an upgradable LiquidationEngine contract")
-  const LiquidationEngine = (await ethers.getContractFactory(
-    "LiquidationEngine",
+  console.log(">> Deploying an upgradable AlpacaStablecoinProxyAction contract")
+  const AlpacaStablecoinProxyAction = (await ethers.getContractFactory(
+    "AlpacaStablecoinProxyAction",
     (
       await ethers.getSigners()
     )[0]
-  )) as LiquidationEngine__factory
-  const liquidationEngine = await upgrades.deployProxy(LiquidationEngine, [BOOK_KEEPER_ADDR, SYSTEM_DEBT_ENGINE_ADDR])
-  await liquidationEngine.deployed()
-  console.log(`>> Deployed at ${liquidationEngine.address}`)
+  )) as AlpacaStablecoinProxyActions__factory
+  const alpacaStablecoinProxyAction = await AlpacaStablecoinProxyAction.deploy()
+  await alpacaStablecoinProxyAction.deployed()
+  console.log(`>> Deployed at ${alpacaStablecoinProxyAction.address}`)
 }
 
 export default func
-func.tags = ["LiquidationEngine"]
+func.tags = ["AlpacaStablecoinProxyAction"]
