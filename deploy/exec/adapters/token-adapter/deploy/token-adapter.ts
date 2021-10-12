bb10844f76ef5fff29ce9460a36d94cdfa6ae8fe
jakeal-papaca <jakeal.papaca@gmail.com>
2021-10-12 11:57:13 +0700

    feat: add TokenAdapter deployment script

---
+++ deploy/exec/adapters/token-adapter/deploy/token-adapter.ts
@@ -0,0 +1,38 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { DeployFunction } from "hardhat-deploy/types"
+import { ethers, upgrades } from "hardhat"
+import { TokenAdapter__factory } from "../../../../../typechain"
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  /*
+  ░██╗░░░░░░░██╗░█████╗░██████╗░███╗░░██╗██╗███╗░░██╗░██████╗░
+  ░██║░░██╗░░██║██╔══██╗██╔══██╗████╗░██║██║████╗░██║██╔════╝░
+  ░╚██╗████╗██╔╝███████║██████╔╝██╔██╗██║██║██╔██╗██║██║░░██╗░
+  ░░████╔═████║░██╔══██║██╔══██╗██║╚████║██║██║╚████║██║░░╚██╗
+  ░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██║░╚███║██║██║░╚███║╚██████╔╝
+  ░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝╚═╝░░╚══╝░╚═════╝░
+  Check all variables below before execute the deployment script
+  */
+
+  const BOOK_KEEPER_ADDR = ""
+  const COLLATERAL_POOL_ID = ""
+  const COLLATERAL_TOKEN_ADDR = ""
+
+  console.log(">> Deploying an upgradable TokenAdapter contract")
+  const TokenAdapter = (await ethers.getContractFactory(
+    "TokenAdapter",
+    (
+      await ethers.getSigners()
+    )[0]
+  )) as TokenAdapter__factory
+  const tokenAdapter = await upgrades.deployProxy(TokenAdapter, [
+    BOOK_KEEPER_ADDR,
+    COLLATERAL_POOL_ID,
+    COLLATERAL_TOKEN_ADDR,
+  ])
+  await tokenAdapter.deployed()
+  console.log(`>> Deployed at ${tokenAdapter.address}`)
+}
+
+export default func
+func.tags = ["TokenAdapter"]
