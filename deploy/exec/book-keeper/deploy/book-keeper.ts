298cb966fc3650653882d49768a5e25fa92b6041
jakeal-papaca <jakeal.papaca@gmail.com>
2021-10-12 11:01:02 +0700

    feat: add bookKeeper deployment script

---
+++ deploy/exec/book-keeper/deploy/book-keeper.ts
@@ -0,0 +1,32 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { DeployFunction } from "hardhat-deploy/types"
+import { ethers, upgrades } from "hardhat"
+import { BookKeeper__factory } from "../../../../typechain"
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  /*
+  ░██╗░░░░░░░██╗░█████╗░██████╗░███╗░░██╗██╗███╗░░██╗░██████╗░
+  ░██║░░██╗░░██║██╔══██╗██╔══██╗████╗░██║██║████╗░██║██╔════╝░
+  ░╚██╗████╗██╔╝███████║██████╔╝██╔██╗██║██║██╔██╗██║██║░░██╗░
+  ░░████╔═████║░██╔══██║██╔══██╗██║╚████║██║██║╚████║██║░░╚██╗
+  ░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██║░╚███║██║██║░╚███║╚██████╔╝
+  ░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝╚═╝░░╚══╝░╚═════╝░
+  Check all variables below before execute the deployment script
+  */
+
+  const COLLATERAL_POOL_CONFIG_ADDR = ""
+
+  console.log(">> Deploying an upgradable BookKeeper contract")
+  const BookKeeper = (await ethers.getContractFactory(
+    "BookKeeper",
+    (
+      await ethers.getSigners()
+    )[0]
+  )) as BookKeeper__factory
+  const bookKeeper = await upgrades.deployProxy(BookKeeper, [COLLATERAL_POOL_CONFIG_ADDR])
+  await bookKeeper.deployed()
+  console.log(`>> Deployed at ${bookKeeper.address}`)
+}
+
+export default func
+func.tags = ["BookKeeper"]
