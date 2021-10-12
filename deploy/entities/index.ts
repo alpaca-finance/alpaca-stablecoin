b1efdbe14229b450c16d155e26d14cf24a60045e
jakeal-papaca <jakeal.papaca@gmail.com>
2021-10-12 14:47:37 +0700

    feat: add config

---
+++ deploy/entities/index.ts
@@ -0,0 +1,5 @@
+import * as ConfigEntity from "./config"
+export { ConfigEntity }
+
+import * as TimelockEntity from "./timelock"
+export { TimelockEntity }
---
+++ deploy/interfaces/config.ts
@@ -0,0 +1,8 @@
+export interface Config {
+  Timelock: string
+  CollateralPoolConfig: CollateralPoolConfig
+}
+
+export interface CollateralPoolConfig {
+  address: string
+}
