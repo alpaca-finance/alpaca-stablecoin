3f7fed96f47e357af62193236eec69de4585bc2c
jakeal-papaca <jakeal.papaca@gmail.com>
2021-10-12 12:01:13 +0700

    feat: add SystemDebtEngine deployment script

---
+++ deploy/exec/system-debt-engine/deploy/system-debt-engine.ts
@@ -0,0 +1,32 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { DeployFunction } from "hardhat-deploy/types"
+import { ethers, upgrades } from "hardhat"
+import { SystemDebtEngine__factory } from "../../../../typechain"
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  /*
+  ░██╗░░░░░░░██╗░█████╗░██████╗░███╗░░██╗██╗███╗░░██╗░██████╗░
+  ░██║░░██╗░░██║██╔══██╗██╔══██╗████╗░██║██║████╗░██║██╔════╝░
+  ░╚██╗████╗██╔╝███████║██████╔╝██╔██╗██║██║██╔██╗██║██║░░██╗░
+  ░░████╔═████║░██╔══██║██╔══██╗██║╚████║██║██║╚████║██║░░╚██╗
+  ░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██║░╚███║██║██║░╚███║╚██████╔╝
+  ░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝╚═╝░░╚══╝░╚═════╝░
+  Check all variables below before execute the deployment script
+  */
+
+  const BOOK_KEEPER_ADDR = ""
+
+  console.log(">> Deploying an upgradable SystemDebtEngine contract")
+  const SystemDebtEngine = (await ethers.getContractFactory(
+    "SystemDebtEngine",
+    (
+      await ethers.getSigners()
+    )[0]
+  )) as SystemDebtEngine__factory
+  const systemDebtEngine = await upgrades.deployProxy(SystemDebtEngine, [BOOK_KEEPER_ADDR])
+  await systemDebtEngine.deployed()
+  console.log(`>> Deployed at ${systemDebtEngine.address}`)
+}
+
+export default func
+func.tags = ["SystemDebtEngine"]
