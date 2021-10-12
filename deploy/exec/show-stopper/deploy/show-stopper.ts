396dc422476abc42af996c0f52b2652426bdb0c6
jakeal-papaca <jakeal.papaca@gmail.com>
2021-10-12 12:04:02 +0700

    feat: add ShowStopper deployment script

---
+++ deploy/exec/show-stopper/deploy/show-stopper.ts
@@ -0,0 +1,30 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { DeployFunction } from "hardhat-deploy/types"
+import { ethers, upgrades } from "hardhat"
+import { ShowStopper__factory } from "../../../../typechain"
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  /*
+  ░██╗░░░░░░░██╗░█████╗░██████╗░███╗░░██╗██╗███╗░░██╗░██████╗░
+  ░██║░░██╗░░██║██╔══██╗██╔══██╗████╗░██║██║████╗░██║██╔════╝░
+  ░╚██╗████╗██╔╝███████║██████╔╝██╔██╗██║██║██╔██╗██║██║░░██╗░
+  ░░████╔═████║░██╔══██║██╔══██╗██║╚████║██║██║╚████║██║░░╚██╗
+  ░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██║░╚███║██║██║░╚███║╚██████╔╝
+  ░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝╚═╝░░╚══╝░╚═════╝░
+  Check all variables below before execute the deployment script
+  */
+
+  console.log(">> Deploying an upgradable ShowStopper contract")
+  const ShowStopper = (await ethers.getContractFactory(
+    "ShowStopper",
+    (
+      await ethers.getSigners()
+    )[0]
+  )) as ShowStopper__factory
+  const showStopper = await upgrades.deployProxy(ShowStopper)
+  await showStopper.deployed()
+  console.log(`>> Deployed at ${showStopper.address}`)
+}
+
+export default func
+func.tags = ["ShowStopper"]
