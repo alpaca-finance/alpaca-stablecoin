68d2398f0baaf4ec680ee1f197a272e611547d43
jakeal-papaca <jakeal.papaca@gmail.com>
2021-10-12 13:45:21 +0700

    feat: add PriceOracle deployment script

---
+++ deploy/exec/price-oracle/deploy/price-oracle.ts
@@ -0,0 +1,32 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { DeployFunction } from "hardhat-deploy/types"
+import { ethers, upgrades } from "hardhat"
+import { PriceOracle__factory } from "../../../../typechain"
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  /*
+  ░██╗░░░░░░░██╗░█████╗░██████╗░███╗░░██╗██╗███╗░░██╗░██████╗░
+  ░██║░░██╗░░██║██╔══██╗██╔══██╗████╗░██║██║████╗░██║██╔════╝░
+  ░╚██╗████╗██╔╝███████║██████╔╝██╔██╗██║██║██╔██╗██║██║░░██╗░
+  ░░████╔═████║░██╔══██║██╔══██╗██║╚████║██║██║╚████║██║░░╚██╗
+  ░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██║░╚███║██║██║░╚███║╚██████╔╝
+  ░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝╚═╝░░╚══╝░╚═════╝░
+  Check all variables below before execute the deployment script
+  */
+
+  const BOOK_KEEPER_ADDR = ""
+
+  console.log(">> Deploying an upgradable PriceOracle contract")
+  const PriceOracle = (await ethers.getContractFactory(
+    "PriceOracle",
+    (
+      await ethers.getSigners()
+    )[0]
+  )) as PriceOracle__factory
+  const priceOracle = await upgrades.deployProxy(PriceOracle, [BOOK_KEEPER_ADDR])
+  await priceOracle.deployed()
+  console.log(`>> Deployed at ${priceOracle.address}`)
+}
+
+export default func
+func.tags = ["PriceOracle"]
