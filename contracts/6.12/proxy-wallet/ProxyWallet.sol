dc49a146aebf10e85af6882c2a70a9f62d0b6da9
ricardo.alpaca <ricardo.alpaca.619@gmail.com>
2021-10-14 21:46:31 +0700

    chore: add licenses to contracts

--- contracts/6.12/proxy-wallet/ProxyWallet.sol
+++ contracts/6.12/proxy-wallet/ProxyWallet.sol
@@ -1,19 +1,15 @@
-// ProxyWallet.sol - execute actions atomically through the proxy's identity
-
-// Copyright (C) 2017  DappHub, LLC
-
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// SPDX-License-Identifier: AGPL-3.0-or-later
+/**
+  ∩~~~~∩ 
+  ξ ･×･ ξ 
+  ξ　~　ξ 
+  ξ　　 ξ 
+  ξ　　 “~～~～〇 
+  ξ　　　　　　 ξ 
+  ξ ξ ξ~～~ξ ξ ξ 
+　 ξ_ξξ_ξ　ξ_ξξ_ξ
+Alpaca Fin Corporation
+*/
 
 pragma solidity 0.6.12;
 
--- contracts/6.12/stablecoin-core/ShowStopper.sol
+++ contracts/6.12/stablecoin-core/ShowStopper.sol
@@ -1,23 +1,15 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-
-/// ShowStopper.sol -- global settlement engine
-
-// Copyright (C) 2018 Rain <rainbreak@riseup.net>
-// Copyright (C) 2018 Lev Livnev <lev@liv.nev.org.uk>
-// Copyright (C) 2020-2021 Maker Ecosystem Growth Holdings, INC.
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Affero General Public License for more details.
-//
-// You should have received a copy of the GNU Affero General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+/**
+  ∩~~~~∩ 
+  ξ ･×･ ξ 
+  ξ　~　ξ 
+  ξ　　 ξ 
+  ξ　　 “~～~～〇 
+  ξ　　　　　　 ξ 
+  ξ ξ ξ~～~ξ ξ ξ 
+　 ξ_ξξ_ξ　ξ_ξξ_ξ
+Alpaca Fin Corporation
+*/
 
 pragma solidity 0.6.12;
 pragma experimental ABIEncoderV2;
