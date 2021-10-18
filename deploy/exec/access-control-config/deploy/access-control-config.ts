d7ea1693540cc455149a41e1fdb4f5ee0fa62c99
jakeal-papaca <jakeal.papaca@gmail.com>
2021-10-18 15:48:48 +0700

    feat: add AccessControlConfig deployment script

---
+++ deploy/exec/access-control-config/deploy/access-control-config.ts
@@ -0,0 +1,30 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { DeployFunction } from "hardhat-deploy/types"
+import { ethers, upgrades } from "hardhat"
+import { AccessControlConfig__factory } from "../../../../typechain"
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  /*
+  ░██╗░░░░░░░██╗░█████╗░██████╗░███╗░░██╗██╗███╗░░██╗░██████╗░
+  ░██║░░██╗░░██║██╔══██╗██╔══██╗████╗░██║██║████╗░██║██╔════╝░
+  ░╚██╗████╗██╔╝███████║██████╔╝██╔██╗██║██║██╔██╗██║██║░░██╗░
+  ░░████╔═████║░██╔══██║██╔══██╗██║╚████║██║██║╚████║██║░░╚██╗
+  ░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██║░╚███║██║██║░╚███║╚██████╔╝
+  ░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝╚═╝░░╚══╝░╚═════╝░
+  Check all variables below before execute the deployment script
+  */
+
+  console.log(">> Deploying an upgradable AccessControlConfig contract")
+  const AccessControlConfig = (await ethers.getContractFactory(
+    "AccessControlConfig",
+    (
+      await ethers.getSigners()
+    )[0]
+  )) as AccessControlConfig__factory
+  const accessControlConfig = await upgrades.deployProxy(AccessControlConfig)
+  await accessControlConfig.deployed()
+  console.log(`>> Deployed at ${accessControlConfig.address}`)
+}
+
+export default func
+func.tags = ["AccessControlConfig"]
