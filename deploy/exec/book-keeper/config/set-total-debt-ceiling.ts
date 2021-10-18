5ea0e6af8206de59809d64cfa49906de3d2844bc
jakeal-papaca <jakeal.papaca@gmail.com>
2021-10-18 15:54:03 +0700

    feat: add setTotalDebtCeiling script

---
+++ deploy/exec/book-keeper/config/set-total-debt-ceiling.ts
@@ -0,0 +1,30 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { DeployFunction } from "hardhat-deploy/types"
+import { ethers, network } from "hardhat"
+import { ConfigEntity } from "../../../entities"
+import { BookKeeper__factory } from "../../../../typechain"
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  /*
+  ░██╗░░░░░░░██╗░█████╗░██████╗░███╗░░██╗██╗███╗░░██╗░██████╗░
+  ░██║░░██╗░░██║██╔══██╗██╔══██╗████╗░██║██║████╗░██║██╔════╝░
+  ░╚██╗████╗██╔╝███████║██████╔╝██╔██╗██║██║██╔██╗██║██║░░██╗░
+  ░░████╔═████║░██╔══██║██╔══██╗██║╚████║██║██║╚████║██║░░╚██╗
+  ░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██║░╚███║██║██║░╚███║╚██████╔╝
+  ░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝╚═╝░░╚══╝░╚═════╝░
+  Check all variables below before execute the deployment script
+  */
+
+  const TOTAL_DEBT_SHARE = "" // [WAD]
+
+  const config = ConfigEntity.getConfig()
+
+  const bookKeeper = BookKeeper__factory.connect(config.BookKeeper.address, (await ethers.getSigners())[0])
+
+  console.log(">> set TOTAL_DEBT_SHARE")
+  await bookKeeper.setTotalDebtCeiling(TOTAL_DEBT_SHARE)
+  console.log("✅ Done")
+}
+
+export default func
+func.tags = ["SetTotalDebtCeiling"]
