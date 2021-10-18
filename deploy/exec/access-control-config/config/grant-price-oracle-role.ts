5d2eea1132ee4e44721243a3a57434839c3db335
jakeal-papaca <jakeal.papaca@gmail.com>
2021-10-18 15:49:40 +0700

    feat: add grant role script

---
+++ deploy/exec/access-control-config/config/grant-price-oracle-role.ts
@@ -0,0 +1,32 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { DeployFunction } from "hardhat-deploy/types"
+import { ethers, network } from "hardhat"
+import { ConfigEntity } from "../../../entities"
+import { AccessControlConfig__factory } from "../../../../typechain"
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  /*
+  ░██╗░░░░░░░██╗░█████╗░██████╗░███╗░░██╗██╗███╗░░██╗░██████╗░
+  ░██║░░██╗░░██║██╔══██╗██╔══██╗████╗░██║██║████╗░██║██╔════╝░
+  ░╚██╗████╗██╔╝███████║██████╔╝██╔██╗██║██║██╔██╗██║██║░░██╗░
+  ░░████╔═████║░██╔══██║██╔══██╗██║╚████║██║██║╚████║██║░░╚██╗
+  ░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██║░╚███║██║██║░╚███║╚██████╔╝
+  ░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝╚═╝░░╚══╝░╚═════╝░
+  Check all variables below before execute the deployment script
+  */
+
+  const PRICE_ORACLE_ADDR = ""
+
+  const config = ConfigEntity.getConfig()
+
+  const accessContralConfig = AccessControlConfig__factory.connect(
+    config.AccessControlConfig.address,
+    (await ethers.getSigners())[0]
+  )
+  console.log(">> Grant PRICE_ORACLE_ROLE")
+  await accessContralConfig.grantRole(await accessContralConfig.PRICE_ORACLE_ROLE(), PRICE_ORACLE_ADDR)
+  console.log("✅ Done")
+}
+
+export default func
+func.tags = ["GrantPriceOracleRole"]
