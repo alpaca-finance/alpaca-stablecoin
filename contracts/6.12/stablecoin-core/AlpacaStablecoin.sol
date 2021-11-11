796f76fa673c78a0daf403cfe48f9fd7d545cc46
ricardo.alpaca <ricardo.alpaca.619@gmail.com>
2021-11-11 14:45:17 +0700

    fix: Peckshield PVE002 ERC Compliance on AlpacaStablecoin

--- contracts/6.12/stablecoin-core/AlpacaStablecoin.sol
+++ contracts/6.12/stablecoin-core/AlpacaStablecoin.sol
@@ -29,7 +29,7 @@ contract AlpacaStablecoin is IStablecoin, AccessControlUpgradeable {
   string public name; // Alpaca USD Stablecoin
   string public symbol; // AUSD
   string public constant version = "1";
-  uint256 public constant override decimals = 18;
+  uint8 public constant override decimals = 18;
   uint256 public totalSupply;
 
   mapping(address => uint256) public override balanceOf;
