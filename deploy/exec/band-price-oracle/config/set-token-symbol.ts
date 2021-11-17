c011822e59a4c59ea4ad0aad212db1b3180e1f90
ricardo.alpaca <ricardo.alpaca.619@gmail.com>
2021-11-17 17:04:07 +0700

    deploy: deploy BandPriceOracle and setTokenSymbol

---
+++ deploy/exec/band-price-oracle/config/set-token-symbol.ts
@@ -0,0 +1,32 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { DeployFunction } from "hardhat-deploy/types"
+import { ethers, network } from "hardhat"
+import { ConfigEntity } from "../../../entities"
+import { BandPriceOracle__factory } from "../../../../typechain"
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  /*
+  ░██╗░░░░░░░██╗░█████╗░██████╗░███╗░░██╗██╗███╗░░██╗░██████╗░
+  ░██║░░██╗░░██║██╔══██╗██╔══██╗████╗░██║██║████╗░██║██╔════╝░
+  ░╚██╗████╗██╔╝███████║██████╔╝██╔██╗██║██║██╔██╗██║██║░░██╗░
+  ░░████╔═████║░██╔══██║██╔══██╗██║╚████║██║██║╚████║██║░░╚██╗
+  ░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██║░╚███║██║██║░╚███║╚██████╔╝
+  ░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝╚═╝░░╚══╝░╚═════╝░
+  Check all variables below before execute the deployment script
+  */
+  const TOKEN_SYMBOL = ""
+  const TOKEN_ADDRESS = ""
+
+  const config = ConfigEntity.getConfig()
+  const bandPriceOracle = BandPriceOracle__factory.connect(
+    config.Oracle.BandPriceOracle.address,
+    (await ethers.getSigners())[0]
+  )
+
+  console.log(">> set TOKEN_SYMBOL")
+  await bandPriceOracle.setTokenSymbol(TOKEN_ADDRESS, TOKEN_SYMBOL)
+  console.log("✅ Done")
+}
+
+export default func
+func.tags = ["SetTokenSymbol"]
