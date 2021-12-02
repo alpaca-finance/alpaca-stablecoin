8b0a3421144691ab4c00c94aa0649af9d6d19bf8
spicysquid168 <spicysquid168@gmail.com>
2021-12-02 16:02:50 +0700

    deploy: alpaca stablecoin to mainnet

--- deploy/exec/collateral-pool-config/config/add-collateral-pool.ts
+++ deploy/exec/collateral-pool-config/config/add-collateral-pool.ts
@@ -41,7 +41,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
       COLLATERAL_POOL_ID: "ibBUSD",
       DEBT_CEILING: ethers.utils.parseUnits("30000000", 45), // 30M [rad]
       DEBT_FLOOR: ethers.utils.parseUnits("100", 45), // 100 [rad]
-      PRICE_FEED: "0x6712262dbA21F3d348f040D87C6779fEB5f56a6B", // ibBUSD IbTokenPriceFeed
+      PRICE_FEED: "0x4a89F897AA97D096dBeA0f874a5854662996f8ae", // ibBUSD IbTokenPriceFeed
       // Collateral Factor for ibBUSD = 99% = 0.99
       // Liquidation Ratio = 1 / 0.99
       LIQUIDATION_RATIO: ethers.utils
@@ -52,7 +52,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
       // Stability Fee Rate to be set = 1000000000627937192491029810
       // Ref: https://www.wolframalpha.com/input/?i=sqrt%281.02%2C+31536000%29
       STABILITY_FEE_RATE: BigNumber.from("1000000000627937192491029810"),
-      ADAPTER: "0x045Ee584B4D7Faa8acdC8e7E7fe869b74889697a", // ibBUSD IbTokenAdapter
+      ADAPTER: "0x4f56a92cA885bE50E705006876261e839b080E36", // ibBUSD IbTokenAdapter
       CLOSE_FACTOR_BPS: BigNumber.from(5000), // 50% Close Factor
       LIQUIDATOR_INCENTIVE_BPS: BigNumber.from(10500), // 5% Liquidator Incentive
       TREASURY_FEES_BPS: BigNumber.from(8000), // 80% Treasury Fee
@@ -67,7 +67,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   for (let i = 0; i < COLLATERAL_POOLS.length; i++) {
     const collateralPool = COLLATERAL_POOLS[i]
     console.log(`>> add CollateralPool ID: ${collateralPool.COLLATERAL_POOL_ID}`)
-    await collateralPoolConfig.initCollateralPool(
+    const tx = await collateralPoolConfig.initCollateralPool(
       formatBytes32String(collateralPool.COLLATERAL_POOL_ID),
       collateralPool.DEBT_CEILING,
       collateralPool.DEBT_FLOOR,
@@ -80,6 +80,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
       collateralPool.TREASURY_FEES_BPS,
       collateralPool.STRATEGY
     )
+    console.log(`tx: ${tx.hash}`)
     console.log(`✅ Done Pool ID: ${collateralPool.COLLATERAL_POOL_ID}`)
   }
 }
--- deploy/exec/ib-token-price-feed/deploy/ib-token-price-feed.ts
+++ deploy/exec/ib-token-price-feed/deploy/ib-token-price-feed.ts
@@ -15,8 +15,8 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   Check all variables below before execute the deployment script
   */
 
-  const IB_IN_BASE_PRICE_FEED_ADDR = "0xDb9B5574581a0e67b119f2c511f7C1e1F3409EC6"
-  const BASE_IN_USD_PRICE_FEED_ADDR = "0xFe104175A2503248438d16C2425d6C2f3FAa5b65"
+  const IB_IN_BASE_PRICE_FEED_ADDR = "0x8CC012356E61424446646ec3B261fe5EF5B44A8a" // AlpacaOraclePriceFeed dddress from 016
+  const BASE_IN_USD_PRICE_FEED_ADDR = "0x9F748f798C75EA44F86a5871045629a2aC9C0568" // StrictAlpacaOraclePriceFeed address from 018
 
   const config = ConfigEntity.getConfig()
 
